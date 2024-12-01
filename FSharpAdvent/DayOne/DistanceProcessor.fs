12ee7f9c9fd8eaabf9e24da76143e08b7a384980
Tollii <tolnes@protonmail.com>
2024-12-01 19:24:50 +0100

    Expand to be more readable

--- FSharpAdvent/DayOne/DistanceProcessor.fs
+++ FSharpAdvent/DayOne/DistanceProcessor.fs
@@ -6,10 +6,16 @@ open FSharpAdvent.DayOne.InputProcessor.InputProcessor
 module DistanceProcessor =
 
     let calculateTotalDistance (locationIds: seq<LocationIdPair>) =
-        locationIds
-        |> Seq.map _.Left
-        |> Seq.sort
-        |> Seq.zip (locationIds
-                    |> Seq.map _.Right
-                    |> Seq.sort)
-        |> Seq.sumBy (fun (left, right) -> Math.Abs (left - right))
+        let calculateDistance (left: int, right: int) = Math.Abs (left - right)
+        
+        let leftIds = locationIds
+                      |> Seq.map _.Left
+                      |> Seq.sort
+                      
+        let rightIds = locationIds
+                       |> Seq.map _.Right
+                       |> Seq.sort
+                     
+        leftIds
+        |> Seq.zip rightIds
+        |> Seq.sumBy calculateDistance
