f0adbc312f79901b2a1116e76e38debe9427efb7
Tollii <tolnes@protonmail.com>
2024-12-02 19:55:25 +0100

    Fix some edge cases

--- FSharpAdvent/DayTwo/Report.fs
+++ FSharpAdvent/DayTwo/Report.fs
@@ -15,36 +15,43 @@ module ReportModule =
     let private decrementValidator (previous, current) = previous > current && isWithinDiffLimit previous current
     
     let private areLevelsValid (levels: int list) =
-        let pairs = List.pairwise levels
-        let isValidIncreasing = List.forall incrementValidator pairs
-        let isValidDecreasing = List.forall decrementValidator pairs
-        isValidIncreasing || isValidDecreasing
+        match levels with 
+        | [] -> true
+        | [_] -> true
+        | lst ->
+            let pairs = List.pairwise lst
+            let isValidIncreasing = List.forall incrementValidator pairs
+            let isValidDecreasing = List.forall decrementValidator pairs
+            isValidIncreasing || isValidDecreasing
             
     let private areLevelsValidWithOneError (levels: int list) =
-        
-        let removeCurrentElement pairwise = 
-            match pairwise with
-            | [] -> []
-            | (first, _) :: tail -> first :: (List.map snd tail)
-            
-        let pairs = List.pairwise levels
-        
-        if incrementValidator pairs.Head then
-            let untilFirstError = pairs |> List.skipWhile incrementValidator
-            let modifiedLevels =
-                match untilFirstError with
-                | [] -> levels
-                | lst -> removeCurrentElement lst
-                
-            areLevelsValid modifiedLevels
-        else
-            let untilFirstError = pairs |> List.skipWhile decrementValidator
-            let modifiedLevels =
-                match untilFirstError with
+        match levels with
+        | [] -> true
+        | [_] -> true
+        | lst ->
+            let removeCurrentElement pairwise = 
+                match pairwise with
                 | [] -> []
-                | lst -> removeCurrentElement lst
+                | (first, _) :: tail -> first :: (List.map snd tail)
                 
-            areLevelsValid modifiedLevels
+            let pairs = List.pairwise lst
+            
+            if incrementValidator pairs.Head then
+                let untilFirstError = pairs |> List.skipWhile incrementValidator
+                let modifiedLevels =
+                    match untilFirstError with
+                    | [] -> levels
+                    | lst -> removeCurrentElement lst
+                    
+                areLevelsValid modifiedLevels
+            else
+                let untilFirstError = pairs |> List.skipWhile decrementValidator
+                let modifiedLevels =
+                    match untilFirstError with
+                    | [] -> []
+                    | lst -> removeCurrentElement lst
+                    
+                areLevelsValid modifiedLevels
         
     let isValid (report: Report) =
         areLevelsValid (report.Levels |> List.ofSeq)
