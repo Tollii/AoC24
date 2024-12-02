8efbd57f6b930052987fa62665331d0c097d3a6f
Tollii <tolnes@protonmail.com>
2024-12-02 22:48:28 +0100

    Trim code, but its still wrong

--- FSharpAdvent/DayTwo/Report.fs
+++ FSharpAdvent/DayTwo/Report.fs
@@ -6,6 +6,10 @@ module ReportModule =
         Levels: int seq
     }
     
+    type LevelDirection =
+        | Increment 
+        | Decrement
+    
     let toReport (parts: string seq) = {
         Levels = parts |> Seq.map int
     }
@@ -13,46 +17,36 @@ module ReportModule =
     let private isWithinDiffLimit previous current = abs (previous - current) <= 3 && abs (previous - current) >= 1
     let private incrementValidator (previous, current) = previous < current && isWithinDiffLimit previous current
     let private decrementValidator (previous, current) = previous > current && isWithinDiffLimit previous current
+    let private direction (pairs: (int * int) list) = if incrementValidator pairs.Head then Increment else Decrement
+    let private areLevelsValidIncr (levels: int list) = levels|> List.pairwise|> List.forall incrementValidator
+    let private areLevelsValidDecr (levels: int list) = levels|> List.pairwise|> List.forall decrementValidator
     
     let private areLevelsValid (levels: int list) =
-        match levels with 
-        | [] -> true
-        | [_] -> true
-        | lst ->
-            let pairs = List.pairwise lst
-            let isValidIncreasing = List.forall incrementValidator pairs
-            let isValidDecreasing = List.forall decrementValidator pairs
-            isValidIncreasing || isValidDecreasing
+        let pairs = List.pairwise levels
+        let isValidIncreasing = List.forall incrementValidator pairs
+        let isValidDecreasing = List.forall decrementValidator pairs
+        isValidIncreasing || isValidDecreasing
             
     let private areLevelsValidWithOneError (levels: int list) =
-        match levels with
-        | [] -> true
-        | [_] -> true
-        | lst ->
-            let removeCurrentElement pairwise = 
-                match pairwise with
-                | [] -> []
-                | (first, _) :: tail -> first :: (List.map snd tail)
-                
-            let pairs = List.pairwise lst
+        let removeCurrentLevel pairwise = 
+            match pairwise with
+            | [] -> []
+            | (first, second) :: tail ->
+                printfn $"Removing {first}, next element is {second}"
+                second :: (List.map snd tail)
             
-            if incrementValidator pairs.Head then
-                let untilFirstError = pairs |> List.skipWhile incrementValidator
-                let modifiedLevels =
-                    match untilFirstError with
-                    | [] -> levels
-                    | lst -> removeCurrentElement lst
-                    
-                areLevelsValid modifiedLevels
-            else
-                let untilFirstError = pairs |> List.skipWhile decrementValidator
-                let modifiedLevels =
-                    match untilFirstError with
-                    | [] -> []
-                    | lst -> removeCurrentElement lst
-                    
-                areLevelsValid modifiedLevels
-        
+        let pairs = List.pairwise levels
+        match direction pairs with
+        | Increment -> 
+            pairs
+            |> List.skipWhile incrementValidator
+            |> removeCurrentLevel
+            |> areLevelsValid
+        | Decrement ->
+            pairs
+            |> List.skipWhile decrementValidator
+            |> removeCurrentLevel
+            |> areLevelsValid
     let isValid (report: Report) =
         areLevelsValid (report.Levels |> List.ofSeq)
         
