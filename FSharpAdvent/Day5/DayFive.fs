8650e240b6593b62d9755f8930f3411ddf6b2a19
Tollii <tolnes@protonmail.com>
2024-12-04 20:46:45 +0100

    Add directory for day five

---
+++ FSharpAdvent/Day5/DayFive.fs
@@ -0,0 +1,14 @@
+module FSharpAdvent.Day5.DayFive
+
+open System.IO
+
+module DayFive =
+    
+    let run =
+        printfn "Day Five"
+        
+        
+        let input =
+            File.ReadLines "4.input"
+        
+        printfn "Wow!"
--- FSharpAdvent/Program.fs
+++ FSharpAdvent/Program.fs
@@ -3,6 +3,7 @@ open FSharpAdvent.Day1.DayOne
 open FSharpAdvent.Day2.DayTwo
 open FSharpAdvent.Day3.DayThree
 open FSharpAdvent.Day4.DayFour
+open FSharpAdvent.Day5.DayFive
 
 printfn "Hello from F#"
 
@@ -10,7 +11,8 @@ let stopwatch = Stopwatch.StartNew()
 // DayOne.run
 // DayTwo.run
 // DayThree.run
-DayFour.run
+// DayFour.run
+DayFive.run
 
 stopwatch.Stop()
 printfn $"Elapsed milliseconds: {stopwatch.Elapsed.Milliseconds}"
